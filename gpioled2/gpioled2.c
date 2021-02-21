b8b2e14c0b6f6b220e334996f108e5c54c201df9
Rüdiger Sonderfeld <ruediger@c-plusplus.net>
2021-02-21 17:39:24 +0000

    gpioled2: Replace magical numbers with consts.

--- gpioled2/gpioled2.c
+++ gpioled2/gpioled2.c
@@ -8,22 +8,26 @@
 #include "pico/stdlib.h"
 
 int main() {
+  const uint LED0 = PICO_DEFAULT_LED_PIN;
+  const uint LED1 = 11;
+  const uint LED2 = 13;
+
   const uint LED_MASK =
-    1ul << PICO_DEFAULT_LED_PIN |  // onboard LED
-    1ul << 11 |
-    1ul << 13;
+    1ul << LED0 |  // onboard LED
+    1ul << LED1 |
+    1ul << LED2;
 
   gpio_init_mask(LED_MASK);
   gpio_set_dir_out_masked(LED_MASK);
-  gpio_clr_mask(1ul << PICO_DEFAULT_LED_PIN | 1ul << 11);
-  gpio_set_mask(1ul << 13);
+  gpio_clr_mask(1ul << LED0 | 1ul << LED1);
+  gpio_set_mask(1ul << LED2);
 
   for (;;) {
-    gpio_xor_mask(1ul << PICO_DEFAULT_LED_PIN | 1ul << 13);
+    gpio_xor_mask(1ul << LED0 | 1ul << LED2);
     sleep_ms(500);
-    gpio_xor_mask(1ul << PICO_DEFAULT_LED_PIN | 1ul << 11);
+    gpio_xor_mask(1ul << LED0 | 1ul << LED1);
     sleep_ms(500);
-    gpio_xor_mask(1ul << 11 | 1ul << 13);
+    gpio_xor_mask(1ul << LED1 | 1ul << LED2);
     sleep_ms(500);
   }
 }
